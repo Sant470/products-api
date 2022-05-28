c626707343a2c4323b399c8aaaacdb9a7cd52781
Sant Purushottam <purushotam.iitr@gmail.com>
2022-05-28 06:36:37 +0530

    feature: add update api

--- data/products.go
+++ data/products.go
@@ -2,10 +2,15 @@ package data
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"time"
 )
 
+// ErrNotFound ...
+var ErrNotFound = fmt.Errorf("Not Found")
+
+// Product ...
 type Product struct {
 	ID          int     `json:"id"`
 	Name        string  `json:"name"`
@@ -16,24 +21,49 @@ type Product struct {
 	UpdatedOn   string  `json:"-"`
 }
 
+// Products ...
 type Products []Product
 
+// FromJSON ...
 func (p *Product) FromJSON(r io.Reader) error {
 	de := json.NewDecoder(r)
 	return de.Decode(p)
 }
 
+// AddProduct ...
 func (p *Product) AddProduct(){
 	p.ID = productList[len(productList)-1].ID + 1
+	fmt.Println("ID: ", p.ID)
 	productList = append(productList, *p)
 }
 
+// UpdateProduct ...
+func (p *Product) UpdateProduct(id int) error {
+	i, err := searchProduct(id)
+	if err != nil {
+		return err
+	}
+	p.ID = id 
+	productList[i] = *p
+	return nil 
+}
+
+func searchProduct(id int) (i int, err error) {
+	for i, prod := range productList {
+		if prod.ID == id {
+			return i, nil
+		}
+	}
+	return -1, ErrNotFound
+} 
+
+// ToJSON ...
 func (ps *Products) ToJSON(w io.Writer) error {
 	ne := json.NewEncoder(w)
 	return ne.Encode(ps)
 }
 
-
+// GetProducts ...
 func GetProducts() Products {
 	return productList
 }
--- handlers/products.go
+++ handlers/products.go
@@ -4,24 +4,45 @@ import (
 	"golang-microservice/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
+// Products ...
 type Products struct {
 	lgr *log.Logger
 }
 
-func NewProducts(lgr *log.Logger) *Products{
+// NewProducts ...
+func NewProducts(lgr *log.Logger) *Products {
 	return &Products{lgr}
 }
 
 func (prd *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
 		prd.getProducts(rw, r)
-		return 
+		return
 	}
 	if r.Method == http.MethodPost {
 		prd.addProducts(rw, r)
-		return 
+		return
+	}
+	if r.Method == http.MethodPut {
+		reg := regexp.MustCompile(`/([0-9]+)`)
+		matches := reg.FindAllStringSubmatch(r.URL.Path, -1)
+		prd.lgr.Println("matches: ", matches)
+		if len(matches) != 1 {
+			http.Error(rw, "Not a valid request!", http.StatusBadRequest)
+		}
+		if len(matches[0]) != 2 {
+			http.Error(rw, "Not a valid regex group!", http.StatusInternalServerError)
+		}
+		id, err := strconv.Atoi(matches[0][1])
+		if err != nil {
+			http.Error(rw, "Not a valid id", http.StatusInternalServerError)
+		}
+		prd.updateProduct(id, rw, r)
+		return
 	}
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
@@ -40,10 +61,24 @@ func (prd *Products) addProducts(rw http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		http.Error(rw, "Could not Decode Body!", http.StatusBadRequest)
 	}
-	prd.lgr.Printf("prod: %#v", prod)
 	prod.AddProduct()
+	prd.lgr.Printf("prod: %#v", prod)
 }
 
-func (prd *Products) updateProduct(rw http.ResponseWriter, r *http.Request) {
-	
+func (prd *Products) updateProduct(id int, rw http.ResponseWriter, r *http.Request) {
+	prod := data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		prd.lgr.Printf("could not decode request :%#v", err)
+	}
+	err = prod.UpdateProduct(id)
+	if err == data.ErrNotFound {
+		http.Error(rw, err.Error(), http.StatusNotFound)
+		return
+	}
+	if err != nil {
+		http.Error(rw, err.Error(), http.StatusInternalServerError)
+		return 
+	}
+	// rw.Write([]byte("successfully updated document!"))
 }
